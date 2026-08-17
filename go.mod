135105c29f5d460fc76d5d614cc0c507010c5711
Lucas Zordan <lucaszordan0204@gmail.com>
2026-08-16 22:46:59 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-iterators
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"iter"
+	"slices"
+)
+
+type Iterator[V any] struct {
+	seq iter.Seq[V]
+}
+
+func From[V any](slice []V) *Iterator[V] {
+	return &Iterator[V]{
+		seq: slices.Values(slice),
+	}
+}
+
+func (it *Iterator[V]) Filter(pred func(V) bool) *Iterator[V] {
+	return &Iterator[V]{
+		seq: func(yield func(V) bool) {
+			for item := range it.seq {
+				if !pred(item) {
+					continue
+				}
+
+				if !yield(item) {
+					return
+				}
+			}
+		},
+	}
+}
+
+func (it *Iterator[V]) Map(mapper func(V) V) *Iterator[V] {
+	return &Iterator[V]{
+		seq: func(yield func(V) bool) {
+			for item := range it.seq {
+				mapped := mapper(item)
+
+				if !yield(mapped) {
+					return
+				}
+			}
+		},
+	}
+}
+
+func (it *Iterator[V]) Collect() []V {
+	return slices.Collect(it.seq)
+}
+
+func main() {
+	result := From([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}).
+		Filter(func(x int) bool { return x%2 == 0 }).
+		Map(func(x int) int { return x * 10 }).
+		Collect()
+
+	fmt.Println(result)
+}
